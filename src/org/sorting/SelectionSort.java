1501385ae931003b3ff4f5799e5dc6a271a1b407
tanujoberoi <tanuj.oberoi@ioptibiz.com>
2017-07-06 12:47:21 +0530

    just setup

---
+++ src/org/sorting/SelectionSort.java
@@ -0,0 +1,12 @@
+package org.sorting;
+
+import javax.sound.midi.SysexMessage;
+
+public class SelectionSort {
+
+	public static void main(String[] args) {
+		System.err.println("hello U !");
+
+	}
+
+}
