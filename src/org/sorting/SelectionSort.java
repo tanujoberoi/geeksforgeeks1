fdf18c3a592a36e015831111125fc2f3140469d1
tanujoberoi <tanuj.oberoi@ioptibiz.com>
2017-07-07 17:12:52 +0530

    selectionsort

--- src/org/sorting/SelectionSort.java
+++ src/org/sorting/SelectionSort.java
@@ -1,12 +1,58 @@
 package org.sorting;
 
-import javax.sound.midi.SysexMessage;
 
 public class SelectionSort {
 
 	public static void main(String[] args) {
 		System.err.println("hello U !");
+		
+		
+		SelectionSort sort = new SelectionSort();
+		int arr[] = {64,25,12,22,11};
+		sort.select(arr);
+		System.out.println("Sorted array");
+		sort.printArray(arr);
 
 	}
+	
+	
+    // Prints the array
+    void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+	
+	int select(int arr[]){
+		System.err.println("start of selection sort .... ");
+		
+		
+		int n = arr.length;
+		
+		// One by one move boundary of unsorted subarray
+        for (int i = 0; i < n-1; i++)
+        {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+ 
+            // Swap the found minimum element with the first
+            // element
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+            
+            printArray(arr);
+        }
+		
+		System.err.println("start of selection sort .... ");
+		return 0;
+	}
+	
+	
 
 }
